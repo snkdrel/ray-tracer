d473519a9eef1882c884a2b3ff249ec9f8c9751f
snkdrel <ces.zop@gmail.com>
2025-05-28 13:46:42 -0600

    Code operations between vectors

--- tests/tuples.cpp
+++ tests/tuples.cpp
@@ -1,4 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
+#include <cmath>
 
 struct Tuple {
     float x;
@@ -49,12 +50,46 @@ Tuple Tuple::operator- () const {
     return {-x, -y, -z, -w};
 }
 
+Tuple operator* (const Tuple& t, const float s) {
+    return {t.x * s, t.y * s, t.z * s, t.w * s};
+}
+
+Tuple operator* (const float s, const Tuple& t) {
+    return {t.x * s, t.y * s, t.z * s, t.w * s};
+}
+
+// check for s == 0
+Tuple operator/ (const Tuple& t, const float s) {
+    return {t.x / s, t.y / s, t.z / s, t.w / s};
+}
+
 Tuple point(float x, float y, float z) {
     return {x, y, z, 1.0};
 }
 
 Tuple vector(float x, float y, float z) {
     return {x, y, z, 0.0};
+}
+
+float magnitude(const Tuple& t) {
+    return std::sqrt(t.x * t.x + t.y * t.y + t.z * t.z + t.w * t.w);
+}
+
+// maybe modify tuple instead of returning a new one?
+// check if magnitude(t) != 0?
+Tuple normalize(const Tuple& t) {
+    float m = magnitude(t);
+    return {t.x / m, t.y / m, t.z / m, t.w / m};
+}
+
+float dot(const Tuple& a, const Tuple& b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+}
+
+Tuple cross(const Tuple& a, const Tuple& b) {
+    return vector(  a.y * b.z - a.z * b.y,
+                    a.z * b.x - a.x * b.z,
+                    a.x * b.y - a.y * b.x );
 }
 
 TEST_CASE ("Distinguish between points and vectors", "[tuple]") {
@@ -134,5 +169,69 @@ TEST_CASE ("Distinguish between points and vectors", "[tuple]") {
         Tuple a {1, -2, 3, -4};
         Tuple expected {-1, 2, -3, 4};
         REQUIRE (-a == expected);
+    }
+
+    SECTION ("Multiplying a tuple by a scalar") {
+        Tuple a {1, -2, 3, -4};
+        Tuple expected {3.5, -7, 10.5, -14};
+        REQUIRE (a * 3.5 == expected);
+    }
+
+    SECTION ("Multiplying a tuple by a fraction") {
+        Tuple a {1, -2, 3, -4};
+        Tuple expected {0.5, -1, 1.5, -2};
+        REQUIRE ((a * 0.5) == expected);
+        REQUIRE ((0.5 * a) == expected);
+    }
+
+    SECTION( "Dividing a tuple by a scalar" ) {
+        Tuple a {1, -2, 3, -4};
+        Tuple expected {0.5, -1, 1.5, -2};
+        REQUIRE((a / 2) == expected);
+    }
+
+    SECTION( "Computing the magnitude of a vector" ) {
+        Tuple v1 = vector(1, 0, 0);
+        REQUIRE(magnitude(v1) == 1);
+
+        Tuple v2 = vector(0, 1, 0);
+        REQUIRE(magnitude(v2) == 1);
+
+        Tuple v3 = vector(0, 0, 1);
+        REQUIRE(magnitude(v3) == 1);
+
+        Tuple v4 = vector(1, 2, 3);
+        REQUIRE(equal(magnitude(v4), std::sqrt(14)));
+
+        Tuple v5 = vector(-1, -2, -3);
+        REQUIRE(equal(magnitude(v5), std::sqrt(14)));
+    }
+
+    SECTION("Normalizing vectors") {
+        Tuple v1 = vector(4, 0, 0);
+        Tuple expected = vector(1, 0, 0);
+        REQUIRE(normalize(v1) == expected);
+
+        Tuple v2 = vector(1, 2, 3);
+        Tuple expected2 = vector(0.26726, 0.53452, 0.80178);
+        REQUIRE(normalize(v2) == expected2);
+
+        Tuple v3 = vector(1, 2, 3);
+        REQUIRE( equal(magnitude(normalize(v3)), 1) );
+    }
+
+    SECTION("The dot product of two tuples") {
+        Tuple a = vector(1, 2, 3);
+        Tuple b = vector(2, 3, 4);
+        REQUIRE(dot(a, b) == 20);
+    }
+
+    SECTION("The cross product of two vectors") {
+        Tuple a = vector(1, 2, 3);
+        Tuple b = vector(2, 3, 4);
+        Tuple expected = vector(-1, 2, -1);
+        Tuple expected2 = vector(1, -2, 1);
+        REQUIRE(cross(a, b) == expected);
+        REQUIRE(cross(b, a) == expected2);
     }
 }
